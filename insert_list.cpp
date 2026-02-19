12e9c84185533586d2bc7aae03c8d1962066c66e
sumit065-dot <rajkishore35798@gmail.com>
2026-02-20 02:25:43 +0530

    value insert at given point

---
+++ insert_list.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){
+	int a[11];
+	int i;
+	printf("Enter element one by one:\n");
+	for(i=0;i<10;i++){
+		scanf("%d",&a[i]);
+	}
+	int val,pos;
+	printf("Enter element to insert:\n");
+	scanf("%d",&val);
+	printf("Enter position to insert:\n");
+	scanf("%d",&pos);
+	for(i=11;i>=pos-1;--i){
+		a[i+1]=a[i];
+	}
+	a[pos-1]=val;
+	printf("----Final----\n");
+	for(i=0;i<11;i++){
+		printf("%d\n",a[i]);
+	}
+}
